54bebcf24cf210d85539a277aab9a700b69efff4
Lenai65209 <97385676+Lenai65209@users.noreply.github.com>
2023-03-25 09:53:25 +0300

    Update articles.py

--- my_blog/views/articles.py
+++ my_blog/views/articles.py
@@ -23,7 +23,10 @@ users_app = Blueprint("users_app", __name__)
 @articles_app.route("/", endpoint="list")
 def articles_list():
     articles = Article.query.all()
-    return render_template("articles/list.html", articles=articles,)
+    count_articles: Dict = requests.get('http://127.0.0.1:5000/api/articles/event_get_count/').json()
+    return render_template("articles/list.html", articles=articles,
+                           count_articles=count_articles['count'],
+                          )
 
 
 @articles_app.route("/<int:article_id>/", endpoint="details")
